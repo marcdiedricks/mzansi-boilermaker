b421588069a2b718bd0d67deced94900eea89281
marcdiedricks <134682072+marcdiedricks@users.noreply.github.com>
2026-08-25 12:55:59 +0200

    Cache KM-03 Lesson 2

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mzansi-boilermaker-v0.1b-km03r-01b';
+const CACHE_NAME = 'mzansi-boilermaker-v0.1b-km03r-02';
 
 const APP_SHELL = [
   './',
@@ -13,6 +13,7 @@ const APP_SHELL = [
   './km02r02.html',
   './km03.html',
   './km03r01.html',
+  './km03r02.html',
   './km04a.html',
   './km04b.html',
   './km04c.html',
