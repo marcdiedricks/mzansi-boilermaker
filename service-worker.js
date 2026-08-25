d0daa7d05a352815ca68c428759294ec70caf892
marcdiedricks <134682072+marcdiedricks@users.noreply.github.com>
2026-08-25 15:35:11 +0200

    Cache synchronized KM-04 menu

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mzansi-boilermaker-v0.1b-km03r-04b';
+const CACHE_NAME = 'mzansi-boilermaker-v0.1b-km04-menu';
 
 const APP_SHELL = [
   './',
@@ -16,6 +16,7 @@ const APP_SHELL = [
   './km03r02.html',
   './km03r03.html',
   './km03r04.html',
+  './km04.html',
   './km04a.html',
   './km04b.html',
   './km04c.html',
